9f2e93b1cd26dfa10f859afce0eaf2e668fde545
rares <roman_rares93@yahoo.com>
2020-12-21 21:21:32 +0200

    add lowerCase to search

--- teams.js
+++ teams.js
@@ -1,4 +1,3 @@
-
 function insertPersons(persons) {
     const tBody = document.querySelector("#list tbody");
     tBody.innerHTML = getPersonsHtml(persons);
@@ -18,8 +17,28 @@ function getPersonHtml(person) {
  </tr>`;
 }
 
+let allPersons = [];
+
 fetch("data/team.json")
     .then(res => res.json())
     .then((data) => {
+        allPersons = data;
         insertPersons(data);
     });
+
+function searchPersons(text) {
+    text = text.toLowerCase();
+    console.log(text);
+    return allPersons.filter(person => {
+        return person.firstName.toLowerCase().indexOf(text) > -1;
+    });
+}
+
+const search = document.getElementById("search");
+search.addEventListener("input", e => {
+    const text = e.target.value;
+
+    const filtrate = searchPersons(text);
+
+    insertPersons(filtrate);
+});
