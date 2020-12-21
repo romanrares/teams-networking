b16e51c57be06e0125935e18cd35bdbe8cfad27a
rares <roman_rares93@yahoo.com>
2020-12-21 20:13:21 +0200

    add some css for table

--- teams.js
+++ teams.js
@@ -1,19 +1,21 @@
 
-function getPersonsHtml(persons) {
-    return persons.map(person =>
-        `<tr>
-            <td>${person.firstName}</td>
-            <td>${person.lastName}</td>
-            <td>
-                <a href= ${person.github}>here</a>
-            </td>
-         </tr>`
-    ).join("");
-}
-
 function insertPersons(persons) {
     const tBody = document.querySelector("#list tbody");
     tBody.innerHTML = getPersonsHtml(persons);
+}
+
+function getPersonsHtml(persons) {
+    return persons.map(getPersonHtml).join("");
+}
+
+function getPersonHtml(person) {
+    return `<tr>
+    <td>${person.firstName}</td>
+    <td>${person.lastName}</td>
+    <td>
+        <a href= ${person.github}>here</a>
+    </td>
+ </tr>`;
 }
 
 fetch("data/team.json")
